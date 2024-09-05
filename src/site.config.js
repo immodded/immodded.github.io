a6d557be668d1b3cd58b785ff0c429c15482d169
immodded <75246338+immodded@users.noreply.github.com>
2024-09-05 14:44:44 +0000

    1.0

---
+++ src/site.config.js
@@ -0,0 +1,9 @@
+const siteConfig = {
+    username:'immodded',
+    title: 'My GitHub Repos',
+    description: 'A list of all my GitHub repositories with links to their websites or GitHub pages.',
+    siteUrl: 'https://immodded.github.io',  // Replace with your actual site URL
+  };
+  
+  export default siteConfig;
+  
